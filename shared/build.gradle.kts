59e34d309afc079ed0e4b4a05012098c3c4afc25
mohitsingh35 <mohitsingh732@yahoo.com>
2024-02-23 00:09:53 +0530

    Final Chages

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
-    id("app.cash.sqldelight") version "2.0.0-alpha05"
+    id("app.cash.sqldelight") version "2.0.0"
 
 }
 
@@ -33,10 +33,10 @@ kotlin {
             implementation(libs.kotlin.test)
         }
         androidMain.dependencies {
-            implementation("app.cash.sqldelight:android-driver:2.0.0-alpha05")
+            implementation("app.cash.sqldelight:android-driver:2.0.0")
         }
         iosMain.dependencies {
-            implementation("app.cash.sqldelight:native-driver:2.0.0-alpha05")
+            implementation("app.cash.sqldelight:native-driver:2.0.0")
         }
 
     }
